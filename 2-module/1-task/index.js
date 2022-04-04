8b31311cba7cfc9d6eff6a3dd531ac28fa450bf9
SergeyZheleznov <jeleznov_sv@mail.ru>
2022-04-04 17:35:03 +0300

    Решение задач 3.1-3.4 дата 04.04.2022

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,23 +1,18 @@
 function sumSalary(salaries) {
   let arraySalary = Object.values(salaries);
-  console.log(arraySalary);
   let arrayNumber = [];
 
   for (let i = 0; i < arraySalary.length; i++) {
     if (typeof arraySalary[i] == "number" && arraySalary[i] !== Infinity && arraySalary[i] !== -Infinity && true !== isNaN(arraySalary[i])) {
       arrayNumber[i] = arraySalary[i];
     }
-    console.log(arrayNumber);
   }
-
-  console.log("type an object value massive", arrayNumber);
 
   if (arrayNumber.length == 0) {
     return 0;
   }
 
   let a = 0;
-  console.log(a);
 
   for (let i = 0; i < arrayNumber.length; i++) {
     a += arrayNumber[i];
